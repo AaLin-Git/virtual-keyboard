4cd28d42fe2cf8bc923403584f18897d9ed0cb40
AaLin-Git <aalin.box.it@gmail.com>
2023-05-01 17:27:55 +0200

    feat: add data for other keys

--- js/keyList.js
+++ js/keyList.js
@@ -69,4 +69,266 @@ export const firstRowKeys = [
     ru: 'Backspace',
     code: 'Backspace',
   },
-]
+];
+
+export const secondRowKeys = [
+  {
+    en: 'Tab',
+    ru: 'Tab',
+    code: 'Tab',
+  },
+  {
+    en: 'q',
+    ru: 'й',
+    code: 'KeyQ',
+  },
+  {
+    en: 'w',
+    ru: 'ц',
+    code: 'KeyW',
+  },
+  {
+    en: 'e',
+    ru: 'у',
+    code: 'KeyE',
+  },
+  {
+    en: 'r',
+    ru: 'к',
+    code: 'KeyR',
+  },
+  {
+    en: 't',
+    ru: 'е',
+    code: 'KeyT',
+  },
+  {
+    en: 'y',
+    ru: 'н',
+    code: 'KeyY',
+  },
+  {
+    en: 'u',
+    ru: 'г',
+    code: 'KeyU',
+  },
+  {
+    en: 'i',
+    ru: 'ш',
+    code: 'KeyI',
+  },
+  {
+    en: 'o',
+    ru: 'щ',
+    code: 'KeyO',
+  },
+  {
+    en: 'p',
+    ru: 'з',
+    code: 'KeyP',
+  },
+  {
+    en: '[',
+    ru: 'х',
+    code: 'BracketLeft',
+  },
+  {
+    en: ']',
+    ru: 'ъ',
+    code: 'BracketRight',
+  },
+  {
+    en: '&bsol;',
+    ru: '&bsol;',
+    code: 'Backslash',
+  },
+  {
+    en: 'Del',
+    ru: 'Del',
+    code: 'NumpadDecimal',
+  },
+];
+
+export const thirdRowKeys = [
+  {
+    en: 'CapsLock',
+    ru: 'CapsLock',
+    code: 'CapsLock',
+  },
+  {
+    en: 'a',
+    ru: 'ф',
+    code: 'KeyA',
+  },
+  {
+    en: 's',
+    ru: 'ы',
+    code: 'KeyS',
+  },
+  {
+    en: 'd',
+    ru: 'в',
+    code: 'KeyD',
+  },
+  {
+    en: 'f',
+    ru: 'а',
+    code: 'KeyF',
+  },
+  {
+    en: 'g',
+    ru: 'п',
+    code: 'KeyG',
+  },
+  {
+    en: 'h',
+    ru: 'р',
+    code: 'KeyH',
+  },
+  {
+    en: 'j',
+    ru: 'о',
+    code: 'KeyJ',
+  },
+  {
+    en: 'k',
+    ru: 'л',
+    code: 'KeyK',
+  },
+  {
+    en: 'l',
+    ru: 'д',
+    code: 'KeyL',
+  },
+  {
+    en: ';',
+    ru: 'ж',
+    code: 'Semicolon',
+  },
+  {
+    en: '&rsquo;',
+    ru: 'э',
+    code: 'Quote',
+  },
+  {
+    en: 'Enter',
+    ru: 'Enter',
+    code: 'Enter',
+  },
+];
+
+export const fourthRowKeys = [
+  {
+    en: 'Shift',
+    ru: 'Shift',
+    code: 'ShiftLeft',
+  },
+  {
+    en: 'z',
+    ru: 'я',
+    code: 'KeyZ',
+  },
+  {
+    en: 'x',
+    ru: 'ч',
+    code: 'KeyX',
+  },
+  {
+    en: 'c',
+    ru: 'с',
+    code: 'KeyC',
+  },
+  {
+    en: 'v',
+    ru: 'м',
+    code: 'KeyV',
+  },
+  {
+    en: 'b',
+    ru: 'и',
+    code: 'KeyB',
+  },
+  {
+    en: 'n',
+    ru: 'т',
+    code: 'KeyN',
+  },
+  {
+    en: 'm',
+    ru: 'ь',
+    code: 'KeyM',
+  },
+  {
+    en: ',',
+    ru: 'б',
+    code: 'Comma',
+  },
+  {
+    en: '.',
+    ru: 'ю',
+    code: 'Period',
+  },
+  {
+    en: '/',
+    ru: '.',
+    code: 'Slash',
+  },
+  {
+    en: '&uarr;',
+    ru: '&uarr;',
+    code: 'ArrowUp',
+  },
+  {
+    en: 'Shift',
+    ru: 'Shift',
+    code: 'ShiftRight',
+  },
+];
+
+export const fifthRowKeys = [
+  {
+    en: 'Ctrl',
+    ru: 'Ctrl',
+    code: 'ControlLeft',
+  },
+  {
+    en: 'Win',
+    ru: 'Win',
+    code: 'MetaLeft',
+  },
+  {
+    en: 'Alt',
+    ru: 'Alt',
+    code: 'AltLeft',
+  },
+  {
+    en: '',
+    ru: '',
+    code: 'Space',
+  },
+  {
+    en: 'Alt',
+    ru: 'Alt',
+    code: 'AltRight',
+  },
+  {
+    en: '&larr;',
+    ru: '&larr;',
+    code: 'ArrowLeft',
+  },
+  {
+    en: '&darr;',
+    ru: '&darr;',
+    code: 'ArrowDown',
+  },
+  {
+    en: '&rarr;',
+    ru: '&rarr;',
+    code: 'ArrowRight',
+  },
+  {
+    en: 'Ctrl',
+    ru: 'Ctrl',
+    code: 'ControlRight',
+  },
+];
